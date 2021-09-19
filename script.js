16e9ca4f3bd7a504e450eb3bfdfc245e44dd5822
Dphan92 <89750029+Dphan92@users.noreply.github.com>
2021-09-18 20:57:20 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function incbutton2(){
+    var element = document.getElementById('increasetext2');
+    var value = element.innerHTML; 
+    ++value
+    document.getElementById('increasetext2').innerHTML = value;
+}
+function incbutton1(){
+    var element = document.getElementById('increasetext1');
+    var value = element.innerHTML; 
+    ++value
+    document.getElementById('increasetext1').innerHTML = value;
+}
+function incbutton3(){
+    var element = document.getElementById('increasetext3');
+    var value = element.innerHTML; 
+    ++value
+    document.getElementById('increasetext3').innerHTML = value;
+}
